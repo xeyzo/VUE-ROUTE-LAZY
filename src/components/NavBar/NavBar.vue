2454474ba3bf54ad78a6b118eb15186c1858de11
xeyzo <schneidergw@yahoo.com>
2020-08-28 08:49:56 +0700

    add navbar

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="navbar">
-    <ul class="flex border-b">
-      <li class="mx-3">
-        <router-link to="/">Post</router-link>
-      </li>
-      <li class="mx-3">
-        <router-link to="/photos">Photo</router-link>
-      </li>
-      <li class="mx-3">
-        <router-link to="/albums">Album</router-link>
-      </li>
-    </ul>
-  </div>
+<ul class="flex">
+  <li class="mr-3">
+    <a class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3"><router-link to="/">Post</router-link></a>
+  </li>
+  <li class="mr-3">
+    <a class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3"><router-link to="/photos">Photo</router-link></a>
+  </li>
+  <li class="mr-3">
+    <a class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3"><router-link to="/albums">Album</router-link></a>
+  </li>
+</ul>
 </template>
 
 <script>
@@ -22,12 +20,3 @@ export default {
 }
 </script>
 
-</template>
-
-<script>
-export default {
-  name: 'NavBar',
-  components: {
-  }
-}
-</script>
